1984fb1e851c868bfa70cb70d19d2fc99e51ff67
ELCOT <ELCOT@192.168.137.173>
2022-11-02 00:47:09 -0400

    swapi

--- hello_world/src/hello_world/Swap.java
+++ hello_world/src/hello_world/Swap.java
@@ -13,15 +13,29 @@ public class Swap {
 	 * 
 	 */
 	public Swap() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+        float first = 1.20f, second = 2.45f;
+        
+        System.out.println("--Before swap--");
+        System.out.println("First number = " + first);
+        System.out.println("Second number = " + second);
+        
+        
+        float temporary = first;
+        first = second;
+        second= temporary;
+        
+        System.out.println("--After swap--");
+        System.out.println("First number = " + first);
+        System.out.println("Second number = " + second);
 
 	}
+	}
 
-}
