059d6a2042af3cb1067879cb4037604184fb6dc1
ELCOT <ELCOT@monisha>
2022-11-14 00:00:12 -0500

    commit push

---
+++ primenumbers/src/primenumbers/Leap.java
@@ -0,0 +1,21 @@
+package primenumbers;
+
+public class Leap {
+
+
+	public static void main(String args[]) {
+		// TODO Auto-generated constructor stub
+		int year = 1900;
+		boolean leap = false;
+		if (year % 4 == 0) {
+			if (year % 400 == 0) 
+				leap = true;
+			else
+				leap = false;
+		if(leap)
+			System.out.println(year + " is a leap year.");
+		else
+			System.out.println(year + " is not a leap year.");
+		}
+	}
+}
